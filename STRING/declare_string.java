eb1c336afa860e7735c40c9859d9209056597756
luckysingh3003 <lucky.399singh@gmail.com>
2025-01-18 13:16:06 +0530

    update

--- STRING/declare_string.java
+++ STRING/declare_string.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class declare_string {
         public static void main(String[] args) {
-                // methods to declare string
+                // methods to declare stri ng
 
             char arr[]={'a','b','c','d'};
             String str1="abcd";
@@ -10,8 +10,21 @@ public class declare_string {
             //Strings are IMUTABLE
 
             Scanner sc =new Scanner(System.in);
-            String name;
-            name=sc.nextLine();
-            System.out.println(name);
+            String firstName;
+            String lastName;
+            firstName=sc.nextLine();
+            lastName=sc.nextLine();
+            System.out.println(firstName);
+            System.out.println(firstName.length());
+        
+            String fullName=firstName + " " +lastName; //concat
+            System.out.println(fullName);
+             
+            //print string 
+
+            System.out.println(fullName.charAt(0));
+            for(int i=0;i<fullName.length();i++){
+                System.out.print(fullName.charAt(i) +" ");
+            }
         }
 }
